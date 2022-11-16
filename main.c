fd5618b61f90194937082817107ea403bf98ff0e
FisaniG <fisanig@gmail.com>
2022-11-16 08:14:37 -0800

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - function main
+ * @argc: get a count of arguments
+ * @argv: get a array of arguments
+ * Return: return EXIT_SUCCESS
+ */
+int main(int argc, char **argv)
+{
+	/* Run command loop. */
+	(void)argc;
+	shell_loop(argv);
+	/* Perform any shutdown/cleanup. */
+	return (EXIT_SUCCESS);
+}
