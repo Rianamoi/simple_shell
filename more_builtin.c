b5db9fcbf612599eca3a7fa4f3ae82f8cb1f63ca
FisaniG <fisanig@gmail.com>
2022-11-16 11:36:49 -0800

    more_builtin.c

---
+++ more_builtin.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+int lsh_eof(char **args);
+/**
+ * lsh_eof - handle end of file
+ * @args: arguements
+ * Return: 0
+ */
+int lsh_eof(char **args)
+{
+	(void)args;
+	return (0);
+}
+
